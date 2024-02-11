ff9be06f002f08be6c511d6d2e5ad3d316b2420d
Sultan Daffa Nusantara <nusantarasulltan@gmail.com>
2024-02-11 23:08:53 +0700

    adjusting navbar and routing

--- components/navigation/NavSide.tsx
+++ components/navigation/NavSide.tsx
@@ -10,6 +10,7 @@ export default function SideNav() {
   const [isDropdownOpen1, setDropdownOpen1] = useState(false);
   const [isDropdownOpen2, setDropdownOpen2] = useState(false);
   const [isDropdownOpen3, setDropdownOpen3] = useState(false);
+  const [isDropdownOpen4, setDropdownOpen4] = useState(false);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -314,17 +315,19 @@ export default function SideNav() {
                 onClick={() => setDropdownOpen3(!isDropdownOpen3)}
               >
                 <svg
-                  className="w-6 h-6  text-white"
-                  aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
+                  viewBox="0 0 36 45"
+                  x="0px"
+                  y="0px"
+                  className="h-7 w-6"
+                  fill="#FFFFFF"
                 >
-                  <path d="M12 7.2c4.4 0 8-1.2 8-2.6C20 3.2 16.4 2 12 2S4 3.2 4 4.6C4 6 7.6 7.2 12 7.2ZM12 22c5 0 8-1.7 8-2.6V15h-.2a7.8 7.8 0 0 1-1.3.7l-.2.1c-2 .7-4.2 1-6.3 1a19 19 0 0 1-6.3-1h-.2a10.1 10.1 0 0 1-1.3-.7L4 15v4.4c0 1 3 2.6 8 2.6Zm7-14c-.1.2-.3.2-.5.3l-.2.1c-2 .7-4.2 1-6.3 1a19 19 0 0 1-6.3-1h-.2a10.2 10.2 0 0 1-1.3-.7L4 7.6V12c0 1 3 2.6 8 2.6s8-1.7 8-2.6V7.6h-.2a7.8 7.8 0 0 1-.7.5Z" />
+                  <path d="M27.66667,22.83333H8.33333a4.83334,4.83334,0,0,0,0,9.66667H27.66667a4.83334,4.83334,0,0,0,0-9.66667Z" />
+                  <path d="M30.08333,20.41667V10.75H26.86111v3.22222H20.88184L16.60311,3.50074V3.5H5.91667V20.41667H30.08333ZM9.13889,6.72222h5.29968l2.96243,7.25H9.13889Z" />
                 </svg>
 
                 <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
-                  Master Data
+                  Data Units
                 </span>
                 <svg
                   className="w-3 h-3"
@@ -348,18 +351,70 @@ export default function SideNav() {
               >
                 <li>
                   <a
-                    href="/master-data/data-unit"
+                    href="/data-units/unit-list"
                     className="flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group  text-white hover:bg-gray-700"
                   >
-                    Data Unit
+                    Unit List
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <button
+                type="button"
+                className="flex items-center w-full p-2 text-base transition duration-75 rounded-lg group  text-white hover:bg-gray-700"
+                aria-controls="dropdown-example"
+                data-collapse-toggle="dropdown-example"
+                onClick={() => setDropdownOpen4(!isDropdownOpen4)}
+              >
+                <svg
+                  className="w-6 h-6  text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 7.2c4.4 0 8-1.2 8-2.6C20 3.2 16.4 2 12 2S4 3.2 4 4.6C4 6 7.6 7.2 12 7.2ZM12 22c5 0 8-1.7 8-2.6V15h-.2a7.8 7.8 0 0 1-1.3.7l-.2.1c-2 .7-4.2 1-6.3 1a19 19 0 0 1-6.3-1h-.2a10.1 10.1 0 0 1-1.3-.7L4 15v4.4c0 1 3 2.6 8 2.6Zm7-14c-.1.2-.3.2-.5.3l-.2.1c-2 .7-4.2 1-6.3 1a19 19 0 0 1-6.3-1h-.2a10.2 10.2 0 0 1-1.3-.7L4 7.6V12c0 1 3 2.6 8 2.6s8-1.7 8-2.6V7.6h-.2a7.8 7.8 0 0 1-.7.5Z" />
+                </svg>
+
+                <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
+                  Data Parts
+                </span>
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+              <ul
+                id="dropdown-example"
+                className={`${isDropdownOpen4 ? "" : "hidden"} py-2 space-y-2`}
+              >
+                <li>
+                  <a
+                    href="/data-parts/part-requests"
+                    className="flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group  text-white hover:bg-gray-700"
+                  >
+                    Part Requests
                   </a>
                 </li>
                 <li>
                   <a
-                    href="/master-data/spare-part"
+                    href="/data-parts/requested-parts"
                     className="flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group  text-white hover:bg-gray-700"
                   >
-                    Spare Part
+                    Requested Parts
                   </a>
                 </li>
               </ul>
